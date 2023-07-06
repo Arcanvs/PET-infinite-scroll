2ec58a051423e8458bdd64b04d16ed2a2398c764
Jose <jose.osorio.herrera@gmail.com>
2023-07-06 00:56:20 -0400

    showloading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,24 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 const Loading: FC = () => {
+    const [classLoading, setClassLoading] = useState<string>('');
+    const clockLoader = ['clock_loader_10', 'clock_loader_20', 'clock_loader_40', 'clock_loader_60', 'clock_loader_80', 'clock_loader_90'];
+    const [countLoading, setCountLoading] = useState<number>(1);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if(clockLoader.length > countLoading) {
+               setCountLoading(countLoading + 1);
+            }else{
+                setCountLoading(1);
+            }
+            setClassLoading(clockLoader[countLoading - 1]);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [classLoading, countLoading]);
   return (
-    <div>loading</div>
+    <div className='loading'>
+        <span className="material-symbols-outlined">{classLoading}</span>
+    </div>
   )
 }
 
